51f3e3dc9d3b2a269ab6804aca894b246fd7a3e2
Mayeexx <148005086+Mayeexx@users.noreply.github.com>
2024-03-21 00:33:01 -0600

    Agregar archivos de proyecto.

---
+++ Models/Nodo.cs
@@ -0,0 +1,13 @@
+﻿namespace MIPARTE.Models
+{
+    public class Nodo
+    {
+
+  
+            public int Posicion { get; set; } // Propiedad para almacenar la posición del nodo
+            public string Valor { get; set; } // Propiedad para almacenar el valor del nodo
+    }
+    
+
+}
+
---
+++ Services/NodoPosicion.cs
@@ -0,0 +1,27 @@
+﻿using MIPARTE.Models;
+using System.Collections.Generic;
+  using System.Linq;
+namespace MIPARTE.Services
+{
+  
+        // Servicio para manipular la lista de nodos
+        public class NodoPosicion
+        {
+            private List<Nodo> nodos = new List<Nodo>(); // Lista para almacenar los nodos
+
+            // Método para obtener todos los nodos
+            public List<Nodo> ObtenerNodos()
+            {
+                return nodos;
+            }
+
+            // Método para agregar un nodo en una posición específica
+            public void AgregarNodoEnPosicion(int posicion, string valor)
+            {
+                nodos.Insert(posicion, new Nodo { Posicion = posicion, Valor = valor });
+            }
+        }
+    
+
+}
+
